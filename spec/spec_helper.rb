faab6f6fcd2e59d5e9a78656b96b2bdcb1197486
volanja <wandervogel.erde@gmail.com>
2013-12-04 01:53:57 +0900

    serverspecがvagrant用だったのでSSHで使用できるように修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'serverspec'
 require 'pathname'
 require 'net/ssh'
 
-include Serverspec::Helper::Ssh
-include Serverspec::Helper::DetectOS
+include SpecInfra::Helper::Ssh
+include SpecInfra::Helper::DetectOS
 
 RSpec.configure do |c|
   if ENV['ASK_SUDO_PASSWORD']
@@ -25,24 +25,7 @@ RSpec.configure do |c|
       c.host  = host
       options = Net::SSH::Config.for(c.host)
       user    = options[:user] || Etc.getlogin
-      
-      vagrant_up = `vagrant up default`
-      config = `vagrant ssh-config default`
-      if config != ''
-        config.each_line do |line|
-          if match = /HostName (.*)/.match(line)
-            c.host = match[1]
-          elsif  match = /User (.*)/.match(line)
-            user = match[1]
-          elsif match = /IdentityFile (.*)/.match(line)
-            options[:keys] =  [match[1].gsub(/"/,'')]
-          elsif match = /Port (.*)/.match(line)
-            options[:port] = match[1]
-          end
-        end
-      end
-    
-      c.ssh   = Net::SSH.start(c.host, user, options)
+      c.ssh   = Net::SSH.start(host, user, options)
     end
   end
 end
