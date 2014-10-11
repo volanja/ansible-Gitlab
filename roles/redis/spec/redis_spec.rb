68b4e124be7d21985baeef1ed27e711859345c30
volanja <wandervogel.erde@gmail.com>
2014-10-12 00:29:31 +0900

    redisのテストを修正した。

--- roles/redis/spec/redis_spec.rb
+++ roles/redis/spec/redis_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
+describe package('redis') do
+  it { should be_installed }
+end
+
 describe service('redis') do
   it { should be_enabled   }
   it { should be_running   }
+end
+
+describe file('/etc/redis.conf') do
+  it { should be_file }
+  it { should contain "bind 127.0.0.1" }
 end
 
 ## network
